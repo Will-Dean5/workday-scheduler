7b27208285ec6e45d3c38c1039203a81174dd593
Will D <will.dean0103@gmail.com>
2022-08-25 18:51:34 -0400

    fixed colors on the text boxes and now they work with the current time

--- README.md
+++ README.md
@@ -2,6 +2,6 @@
 ## Description
 []()
 
-This is a workday scheduler that allows you to type in the text area and hit the save button to the right of it and it will save what you have typed in the text area. depending on what time it is the background color of the text area will change. If it is the current hour the text area will be red if it is a future hour it will be green and if it is a past hour it will be white.
+This is a workday scheduler that allows you to type in the text area and hit the save button to the right of it and it will save what you have typed in the text area. depending on what time it is the background color of the text area will change. If it is the current hour the text area will be red if it is a future hour it will be green and if it is a past hour it will be white. If you enter something in the text area and hit the save icon it will stay there even if you refresh the page.
 
 ## Image of Webpage
--- assets/script.js
+++ assets/script.js
@@ -1,4 +1,9 @@
+// adds date to top of screen
+var currentDay = $('#currentDay').html(moment().format("dddd, MMMM Do"));
 
+
+
+// adds text areas to the page and the for loop changes the color of the text bpx depending on what time it is
 for (var i = 9; i <= 17; i++) {
     var topEl = $('<div class="row" data-id="' + i + '">');
     var idEl = $('<div class="col-1">' + i + '</div>');
@@ -6,14 +11,23 @@ for (var i = 9; i <= 17; i++) {
     var btnEl = $('<button class="col-1 saved fas fa-bookmark"> Save</button>');
     topEl.append(idEl, textEl, btnEl);
     $('.container').append(topEl);
-
+    if (moment().hour() === i) {
+        textEl.addClass('present')
+    }
+    if (moment().hour() < i) {
+        textEl.addClass('future')
+    }
+    else {
+        textEl.addClass('past')
+    }
 }
-
+// saves the user input to local storage
 $('.container').on('click', '.saved', function () {
     var parentId = $(this).parent().attr('data-id');
     var todo = $(this).prev().val();
     localStorage.setItem(parentId, todo);
 });
+
 $('.todo').each(function () {
     var parentId = $(this).parent().attr('data-id');
     var todo = localStorage.getItem(parentId);
@@ -22,14 +36,8 @@ $('.todo').each(function () {
 
 
 
-// adds date to top of screen
+// sets the variable of time to moment which is the current time/day
 var time = moment();
-$("#currentDay").text(time.format("MMM Do, YYYY"));
 
-if (idEl = i) {
-    textEl.addClass('present')
-} else if (idEl <= i) {
-    textEl.addClass('past')
-} else {
-    textEl.addClass('future')
-}
+
+
